758e599f9ac73f33bdd5b9a8a7e1d1409f725387
Swish-Studio <69727137+Swish-Studio@users.noreply.github.com>
2020-08-16 08:56:35 +0100

    Add files via upload

---
+++ Windows 10 Activator.bat
@@ -0,0 +1,40 @@
+@echo off
+echo Press 1 for Professional
+echo Press 2 for Professional N
+echo Press 3 for Professional Workstations
+echo Press 4 for Professional Workstations N
+echo Press 5 for Professional Education
+echo Press 6 to exit
+set /p KEY=
+goto :%KEY%
+:1
+cls
+echo There will be a box press ok when you see it.
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.msguides.com
+slmgr /ato
+:2
+cls
+echo There will be a box press ok when you see it.
+slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+slmgr /skms kms8.msguides.com
+slmgr /ato
+:3
+cls
+echo There will be a box press ok when you see it.
+slmgr /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+slmgr /skms kms8.msguides.com
+slmgr /ato
+:4
+cls
+echo There will be a box press ok when you see it.
+slmgr /ipk 9FNHH-K3HBT-3W4TD-6383H-6XYWF
+slmgr /skms kms8.msguides.com
+slmgr /ato
+:5
+cls
+echo There will be a box press ok when you see it.
+slmgr /ipk 6TP4R-GNPTD-KYYHQ-7B7DP-J447Y
+slmgr /skms kms8.msguides.com
+slmgr /ato
+:6
