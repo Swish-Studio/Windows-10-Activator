39cb7130991a715ead37ac35d9b30b643c396db5
Swish-Studio <69727137+Swish-Studio@users.noreply.github.com>
2020-08-17 13:20:08 +0100

    Add files via upload

---
+++ Windows 10 Activator V1.1.1.bat
@@ -0,0 +1,193 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQImeVUFAlXi
+::ZQ05rAF9IBncCkqN+0xwdVtCHUrTcjra
+::ZQ05rAF9IAHYFVzEqQIHIRVQQxORfDrqRpQf/OHt7v6IsC0=
+::eg0/rx1wNQPfEVWB+kM9LVsJDDOLMm+1EaZcubi7zumTq1scQO0tGA==
+::fBEirQZwNQPfEVWB+kM9LVsJDDOLMm+1EaZcubi7zumTq1scQO0tGA==
+::cRolqwZ3JBvQF1fEqQIRaCxdQgCNK3j6V+Vcyevv5vyGtkIPNA==
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB1RQA2JAES0A5EO4f7+09qSrl0UQN4IcYHf1aOdYLFDqmTnYZ87125VioUJFB44
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+color 0e
+title Windows 10 Activator
+:0
+echo Press 1 for Professional
+echo Press 2 for Professional N
+echo Press 3 for Professional Workstations
+echo Press 4 for Professional Workstations N
+echo Press 5 for Professional Education
+echo Press 6 to exit
+set /p KEY=
+cls
+goto :%KEY%KEY
+:DEVKEY
+cls
+echo Dev Mode on
+echo Press 1 for Professional
+echo Press 2 for Professional N
+echo Press 3 for Professional Workstations
+echo Press 4 for Professional Workstations N
+echo Press 5 for Professional Education
+echo Press 6 to exit
+set /p DEVKEY=
+cls
+goto :%DEVKEY%DEVKEY
+:1KEY
+cls
+echo There will be a box press ok when you see it.
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Finished
+pause
+cls
+goto 0
+:2KEY
+cls
+echo There will be a box press ok when you see it.
+slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Finished
+pause
+cls
+goto 0
+:3KEY
+cls
+echo There will be a box press ok when you see it.
+slmgr /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Finished
+pause
+cls
+goto 0
+:4KEY
+cls
+echo There will be a box press ok when you see it.
+slmgr /ipk 9FNHH-K3HBT-3W4TD-6383H-6XYWF
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Finished
+pause
+cls
+goto 0
+:5KEY
+cls
+echo There will be a box press ok when you see it.
+slmgr /ipk 6TP4R-GNPTD-KYYHQ-7B7DP-J447Y
+slmgr /skms kms8.msguides.com
+slmgr /ato
+echo Finished
+pause
+cls
+goto 0
+:DEVKEY
+echo Dev Mode on
+echo Press 1 for Professional
+echo Press 2 for Professional N
+echo Press 3 for Professional Workstations
+echo Press 4 for Professional Workstations N
+echo Press 5 for Professional Education
+echo Press 6 to exit
+set /p DEVKEY=
+goto :%DEVKEY%DEVKEY
+:1DEVKEY
+echo There will be a box press ok when you see it.
+echo echo There will be a box press ok when you see it.
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+echo slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr /skms kms8.msguides.com
+echo slmgr /skms kms8.msguides.com
+slmgr /ato
+echo slmgr /ato
+echo Finished
+echo echo Finished
+pause
+echo pause
+goto DEVKEY
+:2DEVKEY
+echo There will be a box press ok when you see it.
+echo echo There will be a box press ok when you see it.
+slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+echo slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+slmgr /skms kms8.msguides.com
+echo slmgr /skms kms8.msguides.com
+slmgr /ato
+echo slmgr /ato
+echo Finished
+echo echo Finished
+pause
+echo pause
+goto DEVKEY
+:3DEVKEY
+echo There will be a box press ok when you see it.
+echo echo There will be a box press ok when you see it.
+slmgr /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+echo slmgr /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+slmgr /skms kms8.msguides.com
+echo slmgr /skms kms8.msguides.com
+slmgr /ato
+echo slmgr /ato
+echo Finished
+echo echo Finished
+pause
+echo pause
+goto DEVKEY
+:4DEVKEY
+echo There will be a box press ok when you see it.
+echo echo There will be a box press ok when you see it.
+slmgr /ipk 9FNHH-K3HBT-3W4TD-6383H-6XYWF
+echo slmgr /ipk 9FNHH-K3HBT-3W4TD-6383H-6XYWF
+slmgr /skms kms8.msguides.com
+echo slmgr /skms kms8.msguides.com
+slmgr /ato
+echo slmgr /ato
+echo Finished
+echo echo Finished
+pause
+echo pause
+goto 0
+:5DEVKEY
+echo There will be a box press ok when you see it.
+echo echo There will be a box press ok when you see it.
+slmgr /ipk 6TP4R-GNPTD-KYYHQ-7B7DP-J44Y
+echo slmgr /ipk 6TP4R-GNPTD-KYYHQ-7B7DP-J447Y
+slmgr /skms kms8.msguides.com
+echo slmgr /skms kms8.msguides.com
+slmgr /ato
+echo slmgr /ato
+echo Finished
+echo echo Finished
+pause
+echo pause
+goto DEVKEY
+:6DEVKEY
+echo Program has been closed
+pause
+goto 0
+:6KEY
